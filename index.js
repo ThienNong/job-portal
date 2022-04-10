2f22f68601046f639362ac93378bf7f77bf77191
Nông Thanh Thiên <81373705+ThienNong@users.noreply.github.com>
2022-04-10 20:33:32 +0700

    test code changed

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ import { AppRegistry } from 'react-native'
 import {name as appName} from './app.json'
 import App from './App'
 
+//Test sự thay đổi trong code
 AppRegistry.registerComponent(appName, () => App);
